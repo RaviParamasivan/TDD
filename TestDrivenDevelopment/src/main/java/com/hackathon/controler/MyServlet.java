e4a32b0b68774a8b8df39bbe3dcc3ecf1cc24066
RaviParamasivan <ravi.paramasivam.m@gmail.com>
2015-08-29 12:54:48 -0700

    Update MyServlet.java

--- TestDrivenDevelopment/src/main/java/com/hackathon/controler/MyServlet.java
+++ TestDrivenDevelopment/src/main/java/com/hackathon/controler/MyServlet.java
@@ -17,34 +17,35 @@ public class MyServlet extends HttpServlet {
 	public void doGet(HttpServletRequest request, HttpServletResponse responce) throws IOException {
 		PrintWriter pw = responce.getWriter();
 		responce.setContentType("text/html");
-		System.out.println("userComments          "+request);
+		System.out.println("userComments          " + request);
 		try {
 			ObjectMapper objectMapper = new ObjectMapper();
-			if (request.getParameter("jsonStr") != null) {
-				String type=request.getParameter("type");
-				System.out.println("type   "+type);
-				if(type!= null && type.equals("module")){
-					System.out.println("Test ");
-					DataSource ds= new DataSource();
-					String options=ds.getModuleName();
-					System.out.println("options      "+options);
-					pw.print(options);
-				}else if(type!= null && type.equals("dashboard")){
-					
-				}else{
-					String userComments= request.getParameter("jsonStr");
-					System.out.println("userComments          "+userComments);
-					UserComments comments= objectMapper.readValue(userComments, UserComments.class);
-					
+
+			String type = request.getParameter("type");
+			System.out.println("type   " + type);
+			if (type != null && type.equals("module")) {
+				System.out.println("Test ");
+				DataSource ds = new DataSource();
+				String options = ds.getModuleName();
+				System.out.println("options      " + options);
+				pw.print(options);
+			} else if (type != null && type.equals("dashboard")) {
+
+			} else {
+				if (request.getParameter("jsonStr") != null) {
+					String userComments = request.getParameter("jsonStr");
+					System.out.println("userComments          " + userComments);
+					UserComments comments = objectMapper.readValue(userComments, UserComments.class);
+
 					Healper helper = new Healper();
 					if (helper.updateUserComments(comments)) {
 						pw.print("User comments update successfull");
 					} else {
 						pw.print("Update comments faild, Please submit again later");
-					}					
+					}
 				}
-
 			}
+
 		} catch (Exception e) {
 			pw.print("There is an Exception :" + e);
 			System.out.println("Exception in controler " + e);
